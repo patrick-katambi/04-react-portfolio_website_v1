753619d31007d519f593997fe69f54e65e4a00c3
patrick-katambi <katambipatrick@gmail.com>
2022-01-06 17:48:01 +0000

    react with tailwind css setup

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="h-screen w-screen flex flex-col justify-center items-center bg-primary">
+      <div className='h-1/3 w-1/2 bg-secondary rounded-lg shadow-md flex flex-col justify-center items-center'>
+      <div className='p-9 bg-accent rounded-lg shadow-md'></div>
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    colors: {
+      primary: "#0A192F",
+      secondary: "#CCD6F6",
+      accent: "#F3F2F4",
+    },
+    extend: {},
+  },
+  plugins: [],
+}
