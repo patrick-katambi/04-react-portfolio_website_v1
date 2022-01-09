bf15890c9f7644648b73c7b1f5a97039c30b62ac
patrick-katambi <katambipatrick@gmail.com>
2022-01-09 12:17:33 +0000

    section title margon left remove

--- src/compoents/section_title.jsx
+++ src/compoents/section_title.jsx
@@ -6,7 +6,7 @@ function SectionTitle({ title }) {
       initial={{ x: "-100vw" }}
       animate={{ x: 0 }}
       transition={{ type: "spring", duration: 0.7, delay: 2.8 }}
-      className="mb-[10px] ml-[30px] laptop:ml-0 text-accent text-[100px] font-codeBold opacity-20"
+      className="mb-[10px] ml-0 text-accent text-[100px] font-codeBold opacity-20"
     >
       <h1>{title}</h1>
     </motion.div>
